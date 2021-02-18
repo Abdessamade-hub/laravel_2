6bd679923b13445edb7d7d75b8275c251e1abab9
abdo <abdessamadebenrais60@gmail.com>
2021-02-18 18:09:29 +0100

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="justify-center flex ">
+        <div class="w-6/12 bg-white flex p-6 rounded-lg">
+            Dashboard
+        </div>
+    </div>
+
+@endsection
